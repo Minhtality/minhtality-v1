190037e3d0af2e6f8ff22e345ec34515be635909
minhtality <mtran1712@gmail.com>
2022-01-14 16:42:51 -0800

    feat: Added skeleton nav bar for hero

--- pages/hero.jsx
+++ pages/hero.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import * as styled from '../styles/hero.styled';
+import React, { useState } from 'react';
+import * as Styled from '../styles/hero.styled';
+
+const projects = [
+    {
+        name: 'Model Page Redesign',
+        description: ''
+    }
+]
+
+const HamburgerIcon = ({ isOpen }) => (
+    <Styled.Hamburger isOpen={isOpen}>
+      <div className="bar1" />
+      <div className="bar2" />
+      <div className="bar3" />
+    </Styled.Hamburger>
+  );
 
 export const Hero = () => {
+    const [isOpen, setOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setOpen( e => {
+            const newState = !e;
+            return newState;
+        })
+    }
     return (
-        <div>
-            <styled.Heading>Minh Tran</styled.Heading>
-            <div>Web Developer (Front-End)</div>
-            <div>Los Angeles</div>
-        </div>
+        <Styled.Container>
+            <Styled.Heading><a href="/">Minh Tran</a></Styled.Heading>
+            <Styled.Menu onClick={handleMenuClick}><HamburgerIcon isOpen={isOpen} /></Styled.Menu>
+        </Styled.Container>
     )
 };
 
--- styles/hero.styled.jsx
+++ styles/hero.styled.jsx
@@ -1,7 +1,53 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    max-width: 1440px;
+    grid-template-rows: auto;
+    grid-template-columns: 1fr 1fr;
+    margin: 0 auto;
+    padding: 50px 0 50px 0;
+    box-sizing: border-box;
+`;
+
 export const Heading = styled.div`
-    margin-bottom: 50px;
-    color: red;
-    text-align: center;
+    font-size: 16px;
+    display: block;
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+
+export const Menu = styled.div`
+    text-align: right;
+`;
+
+export const Hamburger = styled.div`
+  display: block;
+  width: 25px;
+
+  div {
+    background-color: #000;
+    content: '';
+    display: block;
+    height: 2px;
+    margin: 3px 0;
+    transition: all 250ms ease-in-out;
+    border-radius: 1px;
+  }
+
+  ${({isOpen}) => isOpen && `
+  .bar1 {
+      transform: translateY(5px)  rotateZ(45deg);
+    }
+    .bar2 {
+      transform: scale(0);
+    }
+    .bar3 {
+      transform: translateY(-5px)  rotateZ(-45deg);
+    }
+  `}
 `;
