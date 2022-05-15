e0ab7d471f1b5bfd84afd69ec71cbee1b1ebb4be
minhtality <mtran1712@gmail.com>
2022-05-14 20:44:26 -0700

    WIP resume

--- components/PDFViewer/index.jsx
+++ components/PDFViewer/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const index = () => {
   return (
-    <div>Hello world</div>
+    <div style={{height: '100vh', width: '100%', overflow:'hidden'}}>
+      <iframe src="/assets/resume.pdf#toolbar=0&navpanes=0&scrollbar=0" width={'100%'} height={'100%'}></iframe>
+    </div>
   )
 }
 
