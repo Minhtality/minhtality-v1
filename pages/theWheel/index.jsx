05c33568aad7cbc8160fce354d5abb78d64422fa
minhtality <mtran1712@gmail.com>
2022-10-02 08:19:51 -0700

    initial commit

---
+++ pages/theWheel/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NUMBER_OF_SHARES = 100;
+
+const index = () => {
+  return (
+    <div>
+        <div>
+          <h2>Debit</h2>
+
+        </div>
+        <div>Credit</div>
+    </div>
+  )
+}
+
+export default index
