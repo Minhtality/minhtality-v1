86f73fdda18b084b1fcac8ea32d1e5665c6b8282
minhtality <mtran1712@gmail.com>
2022-11-25 15:38:45 -0800

    updated prettier config

---
+++ src/components/Card/index.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    background-color: rgb(229,229,229);
+    box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+`
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-
+import * as Styled from "./index.styled";
 interface CardProps {
-	title: string;
-	description: string;
-	thumbnail?: string;
-	avatar?: string;
-	author: string;
-	date: string;
-	tags: string[];
+    title: string;
+    description: string;
+    thumbnail?: string;
+    avatar?: string;
+    author: string;
+    date: string;
+    tags: string[];
 }
 
 const index = ({ title, description }: CardProps) => {
-	return (
-		<div>
-			<h1>{title}</h1>
-			<p>{description}</p>
-		</div>
-	);
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    );
 };
 
 export default index;
