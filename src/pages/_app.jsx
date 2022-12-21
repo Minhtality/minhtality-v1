751d87cbec66c2a32c07bdf22fda3cf822d48a79
minhtality <mtran1712@gmail.com>
2022-12-21 10:14:08 -0800

    updated prettier rule

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,93 +1,98 @@
-import React, { useEffect } from 'react';
-import Head from 'next/dist/shared/lib/head';
-import { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { DarkIcon, LightIcon } from '../../public/icons';
-import { createGlobalStyle } from "styled-components"
+import React, { useEffect } from "react";
+import Head from "next/dist/shared/lib/head";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { DarkIcon, LightIcon } from "../../public/icons";
+import { createGlobalStyle } from "styled-components";
 //TODO: currently hook not working
 
 const lightTheme = {
-  body: 'rgb(242,242,242)',
-  color: '#363537',
-}
+    body: "rgb(242,242,242)",
+    color: "#363537",
+};
 
 const darkTheme = {
-  body: 'rgb(32,33,36)',
-  color: '#FAFAFA',
-}
+    body: "rgb(32,33,36)",
+    color: "#FAFAFA",
+};
 
 export const GlobalStyles = createGlobalStyle`
-  body {
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.color};
-    transition: all 0.25s linear;
-    padding: 0;
-    margin: 0;
-    font-family: 'Raleway', sans-serif;
-    overflow-x: hidden;
-    -ms-overflow-style: none;  /* Internet Explorer 10+ */
-    scrollbar-width: none;  /* Firefox */
-  }
-  body::-webkit-scrollbar {
-    display: none;
-  }
-  svg {
-    fill: ${({ theme }) => theme.color};
-  }
-  button {
-    border: none;
-    padding: 0;
-  }
-`
+    body {
+        background: ${({ theme }) => theme.body};
+        color: ${({ theme }) => theme.color};
+        transition: all 0.25s linear;
+        padding: 0;
+        margin: 0;
+        font-family: 'Raleway', sans-serif;
+        overflow-x: hidden;
+        -ms-overflow-style: none;  /* Internet Explorer 10+ */
+        scrollbar-width: none;  /* Firefox */
+    }
+    body::-webkit-scrollbar {
+        display: none;
+    }
+    svg {
+        fill: ${({ theme }) => theme.color};
+    }
+    button {
+        border: none;
+        padding: 0;
+    }
+`;
 const ToggleTheme = styled.button`
-/* Look into animating svg */
-  position: fixed;
-  top: 10px;
-  right: 10px;
-  background: transparent;
-  cursor: pointer;
-  svg {
-    ${({ theme }) => theme === 'light' ? `
+    /* Look into animating svg */
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    background: transparent;
+    cursor: pointer;
+    svg {
+        ${({ theme }) =>
+            theme === "light"
+                ? `
       fill: #fff;
-      stroke: #363537;` : 
-      `
+      stroke: #363537;`
+                : `
       fill:#fafafa;
       stroke: #fafafa`};
-    stroke-width: 1px;
-  }
+        stroke-width: 1px;
+    }
 `;
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState("light");
 
-  useEffect(() => {
-    const localTheme = localStorage.getItem('theme');
-    setTheme(localTheme);
-  }, []);
+    useEffect(() => {
+        const localTheme = localStorage.getItem("theme");
+        setTheme(localTheme);
+    }, []);
 
+    const toggleTheme = () => {
+        theme === "light"
+            ? (setTheme("dark"), localStorage.setItem("theme", "dark"))
+            : (setTheme("light"), localStorage.setItem("theme", "light"));
+    };
 
-  const toggleTheme = () => {
-    theme === 'light' ? (
-      setTheme('dark'),
-      localStorage.setItem('theme', 'dark')
-    ) : (
-      setTheme('light'),
-      localStorage.setItem('theme', 'light')
-    )
-  };
-
-  return (
-    <>
-      <Head>
-        <title>Minhtality</title>
-      </Head>
-    <div>
-      <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <ToggleTheme theme={theme} onClick={toggleTheme} aria-label="theme toggle">{theme === 'light' ? <DarkIcon/> : <LightIcon/>}</ToggleTheme>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Minhtality</title>
+            </Head>
+            <div>
+                <ThemeProvider
+                    theme={theme === "dark" ? darkTheme : lightTheme}
+                >
+                    <GlobalStyles />
+                    <ToggleTheme
+                        theme={theme}
+                        onClick={toggleTheme}
+                        aria-label="theme toggle"
+                    >
+                        {theme === "light" ? <DarkIcon /> : <LightIcon />}
+                    </ToggleTheme>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </div>
+        </>
+    );
 }
--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,47 +1,47 @@
-import React from 'react';
-import fs from 'fs'
-import * as path from 'path'
-import matter from 'gray-matter'
-import Link from 'next/link'
-import Card from '@components/Card'
-import { MobileBreakpoint, TabletBreakpoint } from '@components/breakpoints';
+import React from "react";
+import fs from "fs";
+import * as path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
+import Card from "@components/Card";
+import { MobileBreakpoint, TabletBreakpoint } from "@components/breakpoints";
 
-export const index = ({posts}) => {
+export const index = ({ posts }) => {
     return (
         <div>
-            {posts.map(({frontmatter, slug}, index) => (
+            {posts.map(({ frontmatter, slug }, index) => (
                 <Link href={`/blogs/${slug}`} key={index}>
-                <Card 
-                    key={index}
-                    title={frontmatter.title}
-                    author={frontmatter.author}
-                    date={frontmatter.date}
-                    thumbnail={frontmatter.thumbnail}
-                />
+                    <Card
+                        key={index}
+                        title={frontmatter.title}
+                        author={frontmatter.author}
+                        date={frontmatter.date}
+                        thumbnail={frontmatter.thumbnail}
+                    />
                 </Link>
             ))}
         </div>
-    )
-}
+    );
+};
 
 export default index;
 
 export const getStaticProps = async () => {
-    const files = fs.readdirSync(path.join('src', 'posts'))
-    const posts = files.map(filename => {
-        const markdownWithMeta = fs.readFileSync(path
-            .join('src','posts', filename))
-        const {data:frontmatter} = matter(markdownWithMeta)
-        
+    const files = fs.readdirSync(path.join("src", "posts"));
+    const posts = files.map((filename) => {
+        const markdownWithMeta = fs.readFileSync(
+            path.join("src", "posts", filename)
+        );
+        const { data: frontmatter } = matter(markdownWithMeta);
+
         return {
             frontmatter,
-            slug: filename.split('.')[0]
-        }
-    })
+            slug: filename.split(".")[0],
+        };
+    });
     return {
         props: {
-            posts
-        }
-    }
-
-}
+            posts,
+        },
+    };
+};
