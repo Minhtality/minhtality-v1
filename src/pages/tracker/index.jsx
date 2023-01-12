2d0e83215394f4afbe28aab061d3aeb0db679ee4
Minh Tran <mtran1712@gmail.com>
2023-01-11 17:48:51 -0800

    POC to pull in positions

--- src/pages/tracker/index.jsx
+++ src/pages/tracker/index.jsx
@@ -2,44 +2,64 @@ import React, { useState, useEffect } from "react";
 import { TDA_CLIENT_ID, TDA_REFRESH_TOKEN } from "@apis/config";
 import { TDAmeritrade } from "@knicola/tdameritrade";
 
+const REDIRECT_URI = "http://localhost";
+const CONSUMER_ID = `${TDA_CLIENT_ID}`;
+
 const index = () => {
     const [code, setCode] = useState("");
-
+    const [account, setAccount] = useState([]);
+    const [showSync, setShowSync] = useState(false);
     const td = new TDAmeritrade({
         apiKey: TDA_CLIENT_ID,
         redirectUri: "http://localhost",
-        sslKey: 'C:\Users\Minhtality\selfsigned.key',
-        sslCert: 'C:\Users\Minhtality\selfsigned.crt',
+        sslKey: "C:UsersMinhtalityselfsigned.key",
+        sslCert: "C:UsersMinhtalityselfsigned.crt",
     });
 
     const getAccessToken = async (code) => {
         const accessToken = await td.getAccessToken(decodeURIComponent(code));
-        localStorage.setItem('refreshToken', accessToken.refresh_token);
-        localStorage.setItem('accessToken', accessToken.access_token);
-    }
-
-    const getRefreshToken = async () => {
-        await td.getAccessToken(TDA_REFRESH_TOKEN);
-    }
-
-    const getAccounts = async () => {
-        let accounts = await fetch("https://api.tdameritrade.com/v1/accounts?fields=positions", {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        }).then((res) => res.json()).then((data) => data[0]);
-
-        
-        console.log('acounts',accounts);
+        localStorage.setItem("refreshToken", accessToken.refresh_token);
+        localStorage.setItem("accessToken", accessToken.access_token);
     };
 
-    
-    const REDIRECT_URI = "http://localhost";
-    const CONSUMER_ID = `${TDA_CLIENT_ID}`;
-    function login() {
+    // get access token from refresh token
+    const refreshAccessToken = async () => {
+        const accessToken = await td.refreshAccessToken(
+            localStorage.getItem("refreshToken")
+        );
+        localStorage.setItem("accessToken", accessToken.access_token);
+    };
 
-        const loginUrl = new URL(`https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&client_id=${encodeURIComponent(CONSUMER_ID)}%40AMER.OAUTHAP`);
-        const loginWindow = window.open(loginUrl, "_blank");
+    const getAccounts = async () => {
+        await fetch(
+            "https://api.tdameritrade.com/v1/accounts?fields=positions",
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem(
+                        "accessToken"
+                    )}`,
+                },
+            }
+        )
+            .then((res) => {
+                if (
+                    res.status === 401 &&
+                    localStorage.getItem("refreshToken")
+                ) {
+                    setShowSync(true);
+                }
+                return res.json();
+            })
+            .then((data) => setAccount(data[0]?.securitiesAccount));
+    };
+
+    function login() {
+        const loginUrl = new URL(
+            `https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURIComponent(
+                REDIRECT_URI
+            )}&client_id=${encodeURIComponent(CONSUMER_ID)}%40AMER.OAUTHAP`
+        );
+        window.open(loginUrl, "_blank");
     }
 
     const submithandler = (e) => {
@@ -47,7 +67,7 @@ const index = () => {
         const code = e.target[0].value;
         setCode(code);
         getAccessToken(code);
-    }
+    };
 
     return (
         <div>
@@ -55,8 +75,10 @@ const index = () => {
             <form onSubmit={submithandler}>
                 <input type="text" placeholder="Enter Auth Code" />
             </form>
+            <pre>{JSON.stringify(account, 0, 2)}</pre>
             <button onClick={login}>Auth</button>
             <button onClick={getAccounts}>Get Accounts</button>
+            {showSync && <button onClick={refreshAccessToken}>Sync</button>}
         </div>
     );
 };
