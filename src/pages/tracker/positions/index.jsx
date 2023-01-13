df77a17672d15b157dc24e92cfc0f733cd50f68c
Minh Tran <mtran1712@gmail.com>
2023-01-12 20:13:22 -0800

    feat: added skeleton table, pending data persistance

--- src/pages/tracker/positions/index.jsx
+++ src/pages/tracker/positions/index.jsx
@@ -3,14 +3,14 @@ import { DataGrid } from "@mui/x-data-grid";
 
 const index = () => {
     const rows = [
-        { id: 1, col1: "1", col2: "World" },
-        { id: 2, col1: "2", col2: "is Awesome" },
-        { id: 3, col1: "3", col2: "is Amazing" },
+        { id: 1, col1: "1", openDate: "1/1/2023" },
+        { id: 2, col1: "2", openDate: "2/1/2023" },
+        { id: 3, col1: "3", openDate: "12/1/2022" },
     ];
 
     const columns = [
         { field: "col1", headerName: "Trade #", width: 75 },
-        { field: "col2", headerName: "Date Opened", width: 150 },
+        { field: "openDate", headerName: "Date Opened", width: 150 },
         { field: "col3", headerName: "Symbol", width: 150 },
         { field: "col4", headerName: "Exp Date", width: 150 },
         { field: "col5", headerName: "Strike(s)", width: 150 },
@@ -20,7 +20,7 @@ const index = () => {
         { field: "col9", headerName: "Net Debit", width: 150 },
         { field: "col10", headerName: "Date Closed", width: 150 },
         { field: "col11", headerName: "P/L", width: 120 },
-        { field: "col12", headerName: "Notes", width: 250 },
+        { field: "col12", headerName: "Notes", width: 250, editable: true },
     ];
 
     return (
