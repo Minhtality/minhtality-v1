e06e4522e1c051e95c7906312eb612a703c1010c
Minh Tran <mtran1712@gmail.com>
2023-02-11 13:10:51 -0800

    feat: added localstorage hook and update tsconfig

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,29 @@
+import { useState, useEffect, useRef} from 'react'
+
+export const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [state, setState] = useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = useRef(key)
+
+  useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
+
+  return [state, setState]
+}
+
